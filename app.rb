09374af31eedd2a2caac88b2f67212cdbbd0d633
dancunrandy <dancunrandy@gmail.com>
2023-02-06 11:17:19 +0300

    completed assignment

--- app.rb
+++ app.rb
@@ -20,4 +20,19 @@ def add num1, num2
     num1 + num2
 end
 
+# This is the equivalent of calling:
+# puts [1, 2, 3].inspect
+p [1, 2, 3]
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+print add(1,3) + add(2,2)
+print "hello worl!d!"
+print "hello world!"
+print "hello world!"
 puts add(1,3) + add(2,2)
+puts "hello world!"
+puts "hello world!"
+puts "hello world!!"
+
+puts("Hello World!\n")
+print("Pass this test, please.")
+p ("[1,2,3]\n")
